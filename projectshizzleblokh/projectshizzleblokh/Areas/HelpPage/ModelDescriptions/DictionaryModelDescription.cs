7d4a69484493f4aa6eada1d7e41e5c7d0dd7305e
yael keemink <yaelkeemink@gmail.com>
2016-03-08 11:09:16 +0100

    startup

---
+++ projectshizzleblokh/projectshizzleblokh/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace projectshizzleblokh.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ projectshizzleblokh/projectshizzleblokh/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace projectshizzleblokh.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
